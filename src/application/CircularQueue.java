104122331c6882267dab10480fcc9fd845fb18f8
Barth001 <alaekwechekwube@gmail.com>
2022-01-02 16:16:06 -1200

    updated queue

--- src/application/CircularQueue.java
+++ src/application/CircularQueue.java
@@ -1,2 +1,82 @@
-package application;public class CircularQueue {
+package application;
+
+public class CircularQueue {
+    int size = 5;
+    int[] arr = null;
+    int front = 0;
+    int rear = -1;
+    int length = 0;
+
+    public CircularQueue(){
+        this.arr = new int[size];
+    }
+    public CircularQueue(int size){
+        this.size = size;
+        this.arr = new int[size];
+    }
+    public int length(){
+        return length;
+    }
+    public boolean isFull(){
+        return length() == size;
+    }
+    public boolean isEmpty(){
+        return length() == 0;
+    }
+    public int front(){
+        if(isEmpty()){
+            return Integer.MIN_VALUE;
+        }
+        return arr[front];
+    }
+    public int rear(){
+        if(isEmpty()){
+            return Integer.MIN_VALUE;
+        }
+        return arr[rear];
+    }
+    public void enqueue(int data){
+        if(isFull()){
+            System.out.println("Queue is filled up");
+            return;
+        }
+        rear = (rear + 1) % size;
+
+        arr[rear] = data;
+        length++;
+        System.out.println(data+" enqueue into queue");
+        System.out.println("Now rear is: "+rear);
+    }
+    public void dequeue(){
+        if(isEmpty()){
+            System.out.println("Queue is empty");
+            return;
+        }
+        int data = arr[front];
+        front = (front + 1) % size;
+        length--;
+        System.out.println(data+" dequeued");
+        System.out.println("Front position is: "+front);
+    }
+
+    public static void main(String[] args){
+        CircularQueue obj = new CircularQueue();
+        obj.enqueue(70);
+        obj.enqueue(74);
+        obj.enqueue(76);
+        obj.enqueue(78);
+        obj.enqueue(80);
+        obj.enqueue(70);
+        obj.dequeue();
+        obj.dequeue();
+        obj.dequeue();
+        obj.dequeue();
+        obj.dequeue();
+        obj.dequeue();
+        System.out.println("Size of queue is: "+obj.length());
+        obj.enqueue(88);
+        obj.enqueue(82);
+        System.out.println("Front queue is: "+obj.front());
+        System.out.println("Rear queue is: "+obj.rear());
+    }
 }
--- src/application/Queue.java
+++ src/application/Queue.java
@@ -1,2 +1,82 @@
-package application;public class Queue {
+package application;
+
+public class Queue {
+    int size = 6;
+    int[] arr = null;
+    int front = 0;
+    int rear = -1;
+    public Queue(){
+        this.arr = new int[size];
+    }
+    public Queue(int size){
+        this.size = size;
+        this.arr = new int[size];
+    }
+    public int length(){
+        return rear+1;
+    }
+    public boolean isFull(){
+        return length() == size;
+    }
+    public boolean isEmpty(){
+        return length() == front;
+    }
+    public void enqueue(int data){
+        if(isFull()){
+            System.out.println("Queue is filled up");
+            return;
+        }
+        arr[++rear] = data;
+        System.out.println(data+ " enqueue into queue");
+    }
+
+    public void dequeue(){
+        if(isEmpty()){
+            System.out.println("Queue is empty");
+            return;
+        }
+        int data = arr[front];
+        for(int i = front; i<rear; i++){
+            arr[i] = arr[i+1];
+        }
+        rear--;
+        System.out.println(data+ " dequeued");
+    }
+    public int rear(){
+        if(isEmpty()){
+            System.out.println("Queue is empty");
+            return Integer.MIN_VALUE;
+        }
+        return arr[rear];
+    }
+    public int front(){
+        if(isEmpty()){
+            System.out.println("Queue is empty");
+            return Integer.MIN_VALUE;
+        }
+        return arr[front];
+    }
+
+    public static void main(String[] args){
+        Queue obj = new Queue();
+        obj.enqueue(70);
+        obj.enqueue(72);
+        obj.enqueue(74);
+        obj.enqueue(75);
+        obj.enqueue(78);
+        obj.enqueue(82);
+        obj.enqueue(70);
+        System.out.println("Front value: "+obj.front());
+        System.out.println("Rear value: "+obj.rear());
+        obj.dequeue();
+        obj.dequeue();
+        obj.dequeue();
+        obj.dequeue();
+        obj.dequeue();
+        obj.dequeue();
+        obj.dequeue();
+        obj.dequeue();
+        System.out.println("Front value: "+obj.front());
+        System.out.println("Rear value: "+obj.rear());
+    }
 }
