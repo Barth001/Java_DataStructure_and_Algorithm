2cfaf2db2f5772c18674b67a2b56f90cce477c8d
Barth001 <alaekwechekwube@gmail.com>
2022-01-06 02:09:34 -1200

    singly linked list update

--- src/application/SinglyLinkedList.java
+++ src/application/SinglyLinkedList.java
@@ -49,6 +49,131 @@ public class SinglyLinkedList {
         }
     }
 
+    public void delete(int index){
+        if(index < 0 || index > size - 1){
+            System.out.println("Index is out of range");
+            return;
+        }
+        Node current = head;
+        Node previous = null;
+        int i = 0;
+
+        while (current != null && i != index){
+            previous = current;
+            current = current.next;
+            i++;
+        }
+        if(previous == null){
+            assert head != null;
+            head = head.next;
+        } else  {
+            assert current != null;
+            previous.next = current.next;
+        }
+        current.next = null;
+        size++;
+        System.out.println(this);
+    }
+
+    public void deleteObject(int data){
+        Node current = head;
+        Node previous = null;
+
+        while (current != null && current.data != data){
+            previous = current;
+            current = current.next;
+        }
+        if(current == null){
+            System.out.println("Data isn't in the system");
+            return;
+        }
+        if(previous == null){
+            head = head.next;
+        } else  {
+            previous.next = current.next;
+        }
+        current.next = null;
+        size++;
+        System.out.println(this);
+
+    }
+
+    public int get(int index){
+        if(index < 0 || index > size - 1){
+            System.out.println("Index is out of range");
+            return Integer.MIN_VALUE;
+        }
+        Node current = find(index);
+        return current.data;
+    }
+
+    private Node find(int index){
+        Node current = head;
+        int i = 0;
+        while (current != null && i != index){
+            current = current.next;
+            i++;
+        }
+        return current;
+    }
+
+    public void set(int data, int index){
+        if(index < 0 || index > size - 1){
+            System.out.println("Index is out of range");
+            return;
+        }
+        Node target = find(index);
+        target.data = data;
+        System.out.println(this);
+    }
+
+    public int indexOf(int data){
+        Node current = head;
+        int i = 0;
+        int index = -1;
+        while (current != null){
+            if(current.data == data){
+                index = i;
+                break;
+            }
+            current = current.next;
+            i++;
+        }
+        return index;
+    }
+
+    public int lastIndexOf(int data){
+        Node current = head;
+        int i = 0;
+        int index = -1;
+        while (current != null){
+            if(current.data == data){
+                index = i;
+            }
+            current = current.next;
+            i++;
+        }
+        return index;
+    }
+
+    public boolean contain(int data){
+        return indexOf(data) != -1;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+
+    public void clear(){
+        head = tail = null;
+        size = 0;
+    }
+
+
     public String toString(){
         StringBuilder sb = new StringBuilder();
         sb.append("[");
@@ -66,7 +191,8 @@ public class SinglyLinkedList {
         sll.add(70);
         sll.add(71);
         sll.add(73);
-        sll.insert(80, 0);
-        sll.insert(78, 3);
+        sll.add(71);
+        sll.clear();
+        System.out.println(sll.isEmpty());
     }
 }
