35e39a3d75455c901620c7b1fee7561d8bdeecba
Barth001 <alaekwechekwube@gmail.com>
2022-01-09 18:50:42 -1200

    Doubly linked list updated 2

--- src/application/DoublyLinkedList.java
+++ src/application/DoublyLinkedList.java
@@ -27,19 +27,42 @@ public class DoublyLinkedList {
         System.out.println(this);
     }
 
+    public void insert(int data, int index){
+        if(index < 0 || index > (size - 1)){
+            System.out.println("Index is out of range");
+        }
+        Node newNode = new Node(data);
+        Node current = findNode(index);
+        Node previousNode = current.prev;
+        if(current == head){
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        } else {
+            previousNode.next = newNode;
+            newNode.prev = previousNode;
+        }
+        newNode.next = current;
+        current.prev = newNode;
+        size++;
+        System.out.println(this);
+    }
+
     public int get(int index){
         if(index < 0 || index > (size - 1)){
             System.out.println("Index is out of range");
             return Integer.MIN_VALUE;
         }
-        int mid = (size - 1)/2;
-        Node target;
-        if(index > mid){
-            target = forwardFind(index);
-        } else {
-            target = backwardFind(index);
-        }
+        Node target = findNode(index);
         return target.data;
+    }
+
+    public Node findNode(int index){
+        int mid = (size - 1)/2;
+        if(index <= mid){
+            return forwardFind(index);
+        }
+        return backwardFind(index);
     }
 
     private Node forwardFind(int index){
@@ -53,11 +76,11 @@ public class DoublyLinkedList {
     }
 
     private Node backwardFind(int index){
-        int i = size - 1;
+        int i = (size - 1);
         Node current = tail;
         while (current != null && i != index){
             current = current.next;
-            i++;
+            i--;
         }
         return current;
     }
@@ -78,6 +101,7 @@ public class DoublyLinkedList {
         dll.add(70);
         dll.add(72);
         dll.add(74);
-        System.out.println(dll.get(0));
+        System.out.println(dll.get(1));
+        dll.insert(78, 0);
     }
 }
